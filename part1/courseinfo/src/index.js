73056b8ebb5a118b577da78486c6b7f5c490335c
Pedro Cristovão <pcristov@gmail.com>
2020-09-16 19:18:02 +0800

    Refactor courseinfo code, remove unused files

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,28 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({name}) => <h1>{name}</h1>
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map((item, index) =><Part part={item.name} exercises={item.exercises} key={index} />)}
+    </div>
+  )
+}
+
+const Part = ({part, exercises}) => <p>{part} {exercises}</p>
+
+const Total = ({parts}) => <p>Number of exercises {parts.reduce((total, part) => total + part.exercises, 0)}</p>
+
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
