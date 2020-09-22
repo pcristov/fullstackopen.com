48cbd0509c4e46e9c9a60288ccb0e14b8291cd37
Pedro Cristovão <pcristov@gmail.com>
2020-09-22 16:56:25 +0800

    Create effect hook to get data from json file

--- part2/notes/src/components/App.js
+++ part2/notes/src/components/App.js
@@ -1,10 +1,22 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './Note'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  }
+
+  // executed immediately after rendering
+  useEffect(hook, [])
 
   const notesToShow = showAll
     ? notes
