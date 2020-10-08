2452762dc58a4b0bc2170806fa66d01304eb93fa
Pedro Cristovão <pcristov@gmail.com>
2020-10-08 16:52:31 +0800

    Save numbers to backend server

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from '../services/persons'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const [filterText, setFilterText] = useState('')
   const [showAll, setShowAll] = useState(true)
 
   const hook = () => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+    .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }
 
@@ -48,10 +48,14 @@ const App = () => {
         name: newName,
         number: newNumber,
       }
-    
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+  
+      personService
+      .create(personObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+      })
     }
   }
 
