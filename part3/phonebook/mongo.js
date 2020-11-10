eef5007f57ecd1602b0e2f1026bc8915aade933e
Pedro Cristovão <pcristov@gmail.com>
2020-11-10 22:04:48 +0800

    Add script that can be used for adding entries to the phonebook, and for listing all of the existing entries in the phonebook

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as argument. Optionally you can also provide a name and number to add a new contact to the db: node mongo.js <password> <name> <number>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://admin:${password}@cluster.su8qm.mongodb.net/phonebook-app?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Contact = mongoose.model('Contact', contactSchema)
+
+if(process.argv.length === 3) {
+  Contact.find({}).then(result => {
+    result.forEach(contact => {
+      console.log(contact)
+    })
+    mongoose.connection.close()
+  })
+}
+
+if(process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const contact = new Contact({
+    name: name,
+    number: number
+  })
+
+  contact.save().then(result => {
+    console.log(`added ${name} ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
